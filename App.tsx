5db3cb3cd654ffd59a7d8766182242eb306f4dbd
josantsil <joseane.silva59@etec.sp.gov.br>
2024-03-25 20:26:25 -0300

    ProvaDudaEJosi

--- App.tsx
+++ App.tsx
@@ -1,10 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const [baseVariavel, setBaseVariavel] = React.useState('');
+  const [alturaVariavel2, setAlturaVariavel] = React.useState('');
+  const calculo = (parseFloat(baseVariavel) * parseFloat(alturaVariavel2)) / 2;
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.field}>Digite a base:  </Text>
+      <TextInput onChangeText={setBaseVariavel} style={styles.txtField} />
+
+      <Text style={styles.field}>Digite altura: </Text>
+      <TextInput onChangeText={setAlturaVariavel} style={styles.txtField} />
+      {!isNaN(calculo) && <Text style={styles.field}>Resposta: '{calculo}' </Text>}
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +24,20 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: 'rgb(131,111,255)',
+
     justifyContent: 'center',
+    paddingHorizontal: 15,
+  },
+  field: {
+    color: '#fff',
+    fontSize: 22,
+  },
+  txtField: {
+    backgroundColor: 'rgb(72,61,139)',
+    borderRadius: 18,
+    width: 'auto',
+    padding: 9,
+    marginTop: 11,
   },
 });
